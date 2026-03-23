5764dc934a7f08972a80a238551a42ab4cec3df3
DuckPanZ <minhtanngo36@gmail.com>
2026-03-23 10:56:45 +0700

    Add multiple files

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import CounterStateful from "./components/CounterStateful";
+import MessageStateless from "./components/MessageStateless";
+import ChildToParent from "./components/ChildToParent";
+import "./App.css"
+function App() {
+  const [parentMessage, setParentMessage] = useState("Chưa có tin nhắn");
+
+  return (
+    <div className="box">
+      <h1>Bài 2 - Demo State & Data Flow trong React</h1>
+      <hr />
+      <h2>1. Stateful Component (Có state)</h2>
+      <CounterStateful />
+      <hr />
+      <h2>2. Stateless Component (Nhận props)</h2>
+      <MessageStateless text="Tôi chỉ nhận props, không có state." />
+      <hr />
+      <h2>3. Giao tiếp giữa component (Child  Parent)</h2>
+      <ChildToParent onSendMessage={setParentMessage} />
+      <p><strong>Tin nhắn nhận từ con:</strong> {parentMessage}</p>
+      <hr/>
+      <h2>4. Luồng dữ liệu một chiều</h2>
+      <p>
+        Dữ liệu luôn đi từ <strong>Cha  Con</strong> thông qua props.
+        Khi cần gửi ngược lên, ta dùng <strong>callback function</strong>.
+      </p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChildToParent.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+function ChildToParent({ onSendMessage }) {
+  const [msg, setMsg] = useState("");
+
+  const sendToParent = () => {
+    onSendMessage(msg);
+    setMsg("");
+  };
+
+  return (
+    <div className="box1">
+      <input
+        value={msg}
+        onChange={(e) => setMsg(e.target.value)}
+        placeholder="Nhập tin nhắn gửi cho cha"
+      />
+      <button onClick={sendToParent} style={{ marginLeft: "10px" }}>
+        Gửi
+      </button>
+    </div>
+  );
+}
+
+export default ChildToParent;
